57066618b2c1878c0dee87ce7e5baf812644af61
Oscar Sierra <oscarguillermosierralozano@gmail.com>
2025-04-03 07:11:37 -0500

    Cambios

--- backend_EasyPark/src/main/java/com/corhuila/backend_EasyPark/models/entity/Reserva.java
+++ backend_EasyPark/src/main/java/com/corhuila/backend_EasyPark/models/entity/Reserva.java
@@ -26,6 +26,35 @@ public class Reserva {
     @Column(nullable = false)
     private Double precio; // Costo total de la reserva
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_At")
+    private Date created_At;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "update_At")
+    private Date update_At;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "deleted_At")
+    private Date deleted_At;
+
+    @PrePersist
+    protected void onCreate() {
+        Date now = new Date();
+        created_At = now;
+        update_At = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        update_At = new Date();
+    }
+
+    @PreRemove
+    protected void onDelete() {
+        deleted_At = new Date();
+    }
+
     @ManyToOne
     @JoinColumn(name = "vehiculo_id", nullable = false)
     private Vehiculo vehiculo;
@@ -79,6 +108,46 @@ public class Reserva {
     }
 
     public void setUsers(Users user) {
+        this.user = user;
+    }
+
+    public Date getCreated_At() {
+        return created_At;
+    }
+
+    public void setCreated_At(Date created_At) {
+        this.created_At = created_At;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDeleted_At() {
+        return deleted_At;
+    }
+
+    public void setDeleted_At(Date deleted_At) {
+        this.deleted_At = deleted_At;
+    }
+
+    public Date getUpdate_At() {
+        return update_At;
+    }
+
+    public void setUpdate_At(Date update_At) {
+        this.update_At = update_At;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
         this.user = user;
     }
 }
--- backend_EasyPark/src/main/java/com/corhuila/backend_EasyPark/models/entity/Vehiculo.java
+++ backend_EasyPark/src/main/java/com/corhuila/backend_EasyPark/models/entity/Vehiculo.java
@@ -18,13 +18,39 @@ public class Vehiculo implements Serializable {
     @Column(length = (100), nullable = false)
     private String tipoVehiculo;
 
-    @Column(name = "Fecha creación")
-    @Temporal(TemporalType.DATE)
-    private Date created_at;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_At")
+    private Date created_At;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "update_At")
+    private Date update_At;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "deleted_At")
+    private Date deleted_At;
+
+    @PrePersist
+    protected void onCreate() {
+        Date now = new Date();
+        created_At = now;
+        update_At = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        update_At = new Date();
+    }
+
+    @PreRemove
+    protected void onDelete() {
+        deleted_At = new Date();
+    }
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false) // Clave foránea
     private Users user;
+
 
     public Long getId() {
         return id;
@@ -50,14 +76,39 @@ public class Vehiculo implements Serializable {
         this.tipoVehiculo = nit;
     }
 
-    public Date getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(Date created_at) {
-        this.created_at = created_at;
-    }
-
     public Users getUsers() { return user; }
+
     public void setUsers(Users user) { this.user = user; }
+
+    public Date getCreated_At() {
+        return created_At;
+    }
+
+    public void setCreated_At(Date created_At) {
+        this.created_At = created_At;
+    }
+
+    public Date getUpdate_At() {
+        return update_At;
+    }
+
+    public void setUpdate_At(Date update_At) {
+        this.update_At = update_At;
+    }
+
+    public Date getDeleted_At() {
+        return deleted_At;
+    }
+
+    public void setDeleted_At(Date deleted_At) {
+        this.deleted_At = deleted_At;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
 }
